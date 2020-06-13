23de0e2f7398734260a7660b833209bf8ff65518
Peter-Schenkels <peter.schenkels@student.hu.nl>
2020-06-13 21:17:33 +0200

    update

---
+++ Server/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+
+namespace Server
+{
+    class Server
+    {
+        [ThreadStatic]
+        public static Socket listener, acc;
+        private static int port = 1998;
+        private static string LocalIP = "127.0.0.1";
+        private static string messageRecieved;
+        private static List<Client> Clients = new List<Client>();
+
+        public class Client
+        { 
+            public Socket Socket { get; set; }
+        }
+        static void CreateServer()
+        {
+            while (true)
+            {
+                acc = listener.Accept();
+                Client client = new Client();
+                client.Socket = acc;
+                Clients.Add(client);
+                Console.WriteLine("Connecting new client");
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(Listeners);
+                Thread thr = new Thread(pts);
+                thr.IsBackground = true;
+                thr.Start(client); //start client processing thread
+                acc = null;
+                client = null;
+            }
+
+        }
+
+        static bool SocketConnected(Socket s)
+        {
+            if(s == null)
+            {
+                return false;
+            }
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+
+
+        static void bindSocket()
+        {
+            Console.WriteLine("Connecting");
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine(LocalIP);
+            listener.Bind(new IPEndPoint(IPAddress.Any, port));
+            listener.Listen(0);
+        }
+        static void Main(string[] args)
+        {
+            bindSocket();
+            CreateServer();
+        }
+
+        static void Listeners(object Object)
+        {
+            Client client = (Client)Object;
+            while (true)
+            {
+                if (SocketConnected(client.Socket))
+                {
+                    Recieve(client.Socket);
+                    Send();
+                }
+                else
+                {
+                    if (listener != null && client != null)
+                    {
+                        listener.Close();
+                        client.Socket.Close();
+                    }
+                }
+            }
+
+        }
+
+        static void Send()
+        {
+            if(messageRecieved != null)
+            {
+                string msg = messageRecieved;
+                byte[] msgBuffer = Encoding.Default.GetBytes(msg);
+                foreach(Client client in Clients)
+                {
+                    client.Socket.Send(msgBuffer, 0, msgBuffer.Length, 0);
+                }
+                
+            }
+            messageRecieved = null;
+
+
+
+        }
+
+        static void Recieve(Socket client)
+        {
+            try
+            {
+                byte[] buffer = new byte[255];
+                int recieved = client.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, recieved);
+                messageRecieved = Encoding.Default.GetString(buffer);
+            }
+            catch
+            {
+                Console.WriteLine("disconnected");
+            }
+
+        }
+    }
+
+
+    class Client
+    {
+
+
+    }
+
+
+}
---
+++ ipmessenger client/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading;
+using AdonisUI.Controls;
+using System.IO;
+
+namespace ipmessenger_client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+
+    public partial class MainWindow : AdonisWindow
+    {
+
+        //private List<string> recievedMessages = new List<string>();
+        private Thread socketThread;
+        private string publicIPAddress;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            publicIPAddress = GetLocalIPAddress();
+
+
+
+        }
+
+        static string GetLocalIPAddress()
+        {
+            String address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                address = stream.ReadToEnd();
+            }
+
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address.Substring(first, last - first);
+
+            return address;
+        }
+
+        public void Socketing()
+        {
+            while (true)
+            {
+                if (SocketConnected(clientSocket))
+                {
+                    Recieve();
+                }
+                else
+                {
+                    if (clientSocket != null)
+                    {
+                        clientSocket.Close();
+                    }
+                    CreateClient();
+                }
+
+            }
+        }
+
+        Socket clientSocket;
+        private int port = 1998;
+        private string LocalIP = "127.0.0.1";
+
+
+        bool SocketConnected(Socket s)
+        {
+            if (s == null)
+            {
+                return false;
+            }
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+
+        void CreateClient()
+        {
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(LocalIP), port);
+                clientSocket.Connect(endPoint);
+            }
+            catch
+            {
+                Console.WriteLine("Connection failed");
+            }
+        }
+        bool Send(string message)
+        {
+
+            if (clientSocket?.Connected == true)
+            {
+                string msg = message;
+                byte[] msgBuffer = Encoding.Default.GetBytes(msg);
+                clientSocket.Send(msgBuffer, 0, msgBuffer.Length, 0);
+                return true;
+            }
+            return false;
+        }
+
+        void Recieve()
+        {
+            if (clientSocket.Connected)
+            {
+                byte[] buffer = new byte[255];
+                int recieved = clientSocket.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, recieved);
+                string recievedMessage = Encoding.Default.GetString(buffer);
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    chatbox.Items.Add(recievedMessage);
+                });
+
+
+            }
+            else
+            {
+                Console.WriteLine("Socket is not connected");
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (socketThread != null)
+                socketThread.Abort();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LocalIP = ipAddressBox.Text;
+            chatbox.Items.Add("Now connecting to ip: " + LocalIP);
+            if (socketThread != null)
+            {
+                socketThread.Abort();
+                clientSocket = null;
+            }
+            socketThread = new Thread(new ThreadStart(Socketing));
+            socketThread.Start();
+
+
+
+        }
+
+        private void Button_Send(object sender, RoutedEventArgs e)
+        {
+            if (!Send(publicIPAddress + ": " + messageBox.Text ))
+            {
+                chatbox.Items.Add("Socket is not connected");
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            chatbox.Items.Clear();
+        }
+    }
+}
